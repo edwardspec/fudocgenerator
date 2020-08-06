313c509a246d1f04c520d3bbe98e83def327a4fd
Edward Chernenko <edwardspec@gmail.com>
2020-08-06 03:03:59 +0300

    (refactor) Provide RecipeDatabase to decouple parsing and searching
    The input (*.config and *.object files of the mod) has different
    formats for extractors, smelters and centrifuges.
    
    However, to keep our code clean,
    we should have the same standartized "search in what we found" methods
    for all recipes (regardless of whether it's extraction, crafting, etc.)
    
    For that we provide add() method, which can be used by parsers
    of *.config and *.object files to "register" found recipes in the DB.
    
    Sanity checks (and Recipe class that does them) have been added to
    ensure that incorrect recipes are not added into the database.

--- generate.js
+++ generate.js
@@ -11,6 +11,8 @@ var config = require( './config.json' ),
 	fs = require( 'fs' ),
 	vm = require( 'vm' );
 
+var RecipeDatabase = require( './lib/RecipeDatabase' );
+
 /**
  * Load the contents of one *.config/*.object file from the Starbound mod.
  * Returns the parsed structure.
@@ -31,66 +33,47 @@ var centrifugeConf = loadModFile( 'objects/generic/centrifuge_recipes.config' ),
 	arcSmelterConf = loadModFile( 'objects/power/isn_arcsmelter/isn_arcsmelter.object' ),
 	mixerConf = loadModFile( 'objects/power/fu_liquidmixer/fu_liquidmixer_recipes.config' );
 
-// Loop through ALL processing recipes and build 2 arrays: processedIntoMap and obtainedFromMap.
-// Their format is: { codeOfItemOrMaterial: { buildingThatDoesProcessing: RECIPE },
-// where RECIPE is { codeOfItem1: count1, codeOfItem2: count2, ... }.
-// 1) Example for processedIntoMap:
-// { liquidwater: { 'Extraction Lab': { fu_oxygen: 1, fu_hydrogen: 2, fu_salt: 1 } } }
-// 2) Format of "obtainedFromMap" is TO BE DETERMINED (when Liquid Mixer recipes are supported).
-
-var processedIntoMap = {}, obtainedFromMap = {};
 
 /*-------------------------------------------------------------------------------------------- */
-/*---------------- Step 1: Extractors -------------------------------------------------------- */
+/* Step 1: Add recipes from Extractors into RecipeDatabase ----------------------------------- */
 /*-------------------------------------------------------------------------------------------- */
 
-// Array of Extractor names. Should be in the same order as outputs in [extractionlab_recipes.config].
-var extractorStageBuildings = [ 'Extraction Lab', 'Extraction Lab MK II', 'Quantum Extractor' ];
+/**
+ * Helper function: reduces the array like { "carbon": 3, "oxygen": [ 1, 4, 9 ] }
+ * into { "carbon": 3, "oxygen": 1 } for extractorStage=0,
+ * into { "carbon": 3, "oxygen": 4 } for extractorStage=1, etc.
+ */
+function getStageValues( valuesArray, extractorStage ) {
+	var valuesForThisStage = {};
+
+	for ( var [ itemName, counts ] of Object.entries( valuesArray ) ) {
+		valuesForThisStage[itemName] =
+			Number.isInteger( counts ) ? counts : counts[extractorStage];
+	}
+
+	return valuesForThisStage;
+}
 
 for ( var extractorRecipe of extractorConf ) {
-	var inputs = Object.keys( extractorRecipe.inputs )
-	if ( inputs.length !== 1 ) {
-		// NOTE: recipes with more than 1 input currently don't work (in FU itself),
-		// so no need to support them for now.
-		continue;
-	}
+	var outputs = extractorRecipe.outputs;
 
-	var input = inputs[0],
-		outputs = extractorRecipe.outputs;
-
-	// Here we group the "extraction results" by the Stage of the Extractor (its index
-	// in "extractorStageBuildings" array).
-	// For example, for Cookied Fish (gives 3 Research on stages 0/1, but 6 Research on stage 2):
-	// [ { fuscienceresource: 3 }, { fuscienceresource: 3 }, { fuscienceresource: 6 } ]
-	var stageOutput = [];
-
-	for ( var [ outputItem, counts ] of Object.entries( outputs ) ) {
-		// Some recipes (e.g. Iron Block -> Iron Bar) have only 1 count instead of an array of 3 counts.
-		// Normalize the "counts" variable to be an array.
-		if ( Number.isInteger( counts ) ) {
-			counts = extractorStageBuildings.map( () => counts );
-		}
-
-		for ( var [ extractorStage, count ] of Object.entries( counts ) ) {
-			if ( !stageOutput[extractorStage] ) {
-				stageOutput[extractorStage] = {};
-			}
-
-			stageOutput[extractorStage][outputItem] = count;
-		}
-	}
-
-	// Add this recipe to "processedIntoMap" map.
-	if ( !processedIntoMap[input] ) {
-		processedIntoMap[input] = {};
-	}
-
-	extractorStageBuildings.forEach( function ( buildingName, extractorStage ) {
-		processedIntoMap[input][buildingName] = stageOutput[extractorStage];
+	config.extractorStageBuildings.forEach( function ( buildingName, extractorStage ) {
+		RecipeDatabase.add(
+			buildingName,
+			getStageValues( extractorRecipe.inputs, extractorStage ),
+			getStageValues( extractorRecipe.outputs, extractorStage )
+		);
 	} );
-
-	// TODO: update "obtainedFromMap" too.
 }
 
 
-console.log( processedIntoMap );
+// TODO: add recipes from other Stations.
+
+
+
+
+
+
+/*-------------------------------------------------------------------------------------------- */
+
+RecipeDatabase.dump();
