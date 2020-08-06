2cb4b7a9e5da7d9e43a07b3b880373e83a6a7e15
Edward Chernenko <edwardspec@gmail.com>
2020-08-06 04:43:04 +0300

    (refactor) Move minor utility functions into lib/util.js

--- generate.js
+++ generate.js
@@ -8,51 +8,22 @@
  */
 
 var config = require( './config.json' ),
-	fs = require( 'fs' ),
-	vm = require( 'vm' );
+	RecipeDatabase = require( './lib/RecipeDatabase' ),
+	util = require( './lib/util' );
 
-var RecipeDatabase = require( './lib/RecipeDatabase' );
-
-/**
- * Load the contents of one *.config/*.object file from the Starbound mod.
- * Returns the parsed structure.
- */
-function loadModFile( filename ) {
-	var jsSourceCode = fs.readFileSync( config.pathToMod + '/' + filename );
-
-	// This config is fully functional JavaScript code (not JSON), with comments and all,
-	// but without the necessary "module.exports =", so require() can't be used.
-	return vm.runInNewContext( '_ThisVariableIsNeverUsed = ' + jsSourceCode );
-}
 
 // Load configs of all processing stations.
 // NOTE: centrifugeConf covers not only centrifuges, but also powder sifters, honey jars, etc.
-var centrifugeConf = loadModFile( 'objects/generic/centrifuge_recipes.config' ),
-	extractorConf = loadModFile( 'objects/generic/extractionlab_recipes.config' ),
-	blastFurnaceConf = loadModFile( 'objects/power/fu_blastfurnace/fu_blastfurnace.object' ),
-	arcSmelterConf = loadModFile( 'objects/power/isn_arcsmelter/isn_arcsmelter.object' ),
-	mixerConf = loadModFile( 'objects/power/fu_liquidmixer/fu_liquidmixer_recipes.config' );
+var centrifugeConf = util.loadModFile( 'objects/generic/centrifuge_recipes.config' ),
+	extractorConf = util.loadModFile( 'objects/generic/extractionlab_recipes.config' ),
+	blastFurnaceConf = util.loadModFile( 'objects/power/fu_blastfurnace/fu_blastfurnace.object' ),
+	arcSmelterConf = util.loadModFile( 'objects/power/isn_arcsmelter/isn_arcsmelter.object' ),
+	mixerConf = util.loadModFile( 'objects/power/fu_liquidmixer/fu_liquidmixer_recipes.config' );
 
 
 /*-------------------------------------------------------------------------------------------- */
 /* Step 1: Add recipes from Extractors into RecipeDatabase ----------------------------------- */
 /*-------------------------------------------------------------------------------------------- */
-
-/**
- * Helper function: reduces the array like { "carbon": 3, "oxygen": [ 1, 4, 9 ] }
- * into { "carbon": 3, "oxygen": 1 } for extractorStage=0,
- * into { "carbon": 3, "oxygen": 4 } for extractorStage=1, etc.
- */
-function getStageValues( valuesArray, extractorStage ) {
-	var valuesForThisStage = {};
-
-	for ( var [ itemName, counts ] of Object.entries( valuesArray ) ) {
-		valuesForThisStage[itemName] =
-			Number.isInteger( counts ) ? counts : counts[extractorStage];
-	}
-
-	return valuesForThisStage;
-}
 
 for ( var extractorRecipe of extractorConf ) {
 	var outputs = extractorRecipe.outputs;
@@ -60,8 +31,8 @@ for ( var extractorRecipe of extractorConf ) {
 	config.extractorStageBuildings.forEach( function ( buildingName, extractorStage ) {
 		RecipeDatabase.add(
 			buildingName,
-			getStageValues( extractorRecipe.inputs, extractorStage ),
-			getStageValues( extractorRecipe.outputs, extractorStage )
+			util.getStageValues( extractorRecipe.inputs, extractorStage ),
+			util.getStageValues( extractorRecipe.outputs, extractorStage )
 		);
 	} );
 }
