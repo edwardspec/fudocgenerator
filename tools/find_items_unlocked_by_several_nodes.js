1e511c844d477cbbe080589561442af18c993adc
Edward Chernenko <edwardspec@gmail.com>
2020-11-09 03:14:25 +0300

    Add tool [find_items_unlocked_by_several_nodes.js]

---
+++ tools/find_items_unlocked_by_several_nodes.js
@@ -0,0 +1,38 @@
+/**
+ * Find all items that are unlocked by more than one ResearchTree node
+ * (excluding race-specific default unlocks).
+ */
+
+'use strict';
+
+const { ResearchTreeDatabase } = require( '../lib' );
+
+// Format: { itemCode => [ node1, node2, ... ], ... }
+var nodesThatUnlock = {};
+
+ResearchTreeDatabase.forEach( ( node ) => {
+	if ( node.tree == 'No' && node.name != 'Default unlocks' ) {
+		// We ignore race-specific default unlocks,
+		// because it's not an error if these items are also unlocked via the regular ResearchTree.
+		return;
+	}
+
+	for ( var itemCode of node.unlocks ) {
+		if ( !nodesThatUnlock[itemCode] ) {
+			nodesThatUnlock[itemCode] = [];
+		}
+
+		nodesThatUnlock[itemCode].push( node );
+	}
+} );
+
+for ( var [ itemCode, nodes ] of Object.entries( nodesThatUnlock ) ) {
+	if ( nodes.length < 2 ) {
+		// Only unlocked by one node.
+		continue;
+	}
+
+	console.log( itemCode + ': ' + nodes.map( ( node ) => {
+		return node.name + ' (' + node.tree + ' tree)';
+	} ).join( ', ' ) );
+}
