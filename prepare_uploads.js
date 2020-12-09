6f5c90069d43813fea67f15f59573a9a0dcacc15
Edward Chernenko <edwardspec@gmail.com>
2020-12-09 03:52:09 +0300

    Uploads: use ImageMagick to unpack sprites with "frameGrid" option

--- prepare_uploads.js
+++ prepare_uploads.js
@@ -9,6 +9,7 @@
 const { ItemDatabase, RecipeDatabase, ResearchTreeDatabase,
 	WikiStatusCache, config, util } = require( './lib' ),
 	fs = require( 'fs' ),
+	childProcess = require( 'child_process' ),
 	nodePath = require( 'path' );
 
 /*-----------------------------------------------------------------------------------------------*/
@@ -21,6 +22,99 @@ fs.mkdirSync( outputPath + '/all', { recursive: true } );
 fs.mkdirSync( outputPath + '/onlyNew', { recursive: true } );
 
 /*-----------------------------------------------------------------------------------------------*/
+
+// Cache used by unpackSprite().
+// Format: { "filenameOfSprite1": result1, ... }.
+var unpackedSprites = {};
+
+/**
+ * Use ImageMagick (convert tool) to cut the sprite (images glued into one) into individual images.
+ * @param {string} absolutePathToSprite
+ * @return {Object} Format: { "codeOfFrame1": filename1, "codeOfFrame2": filename2, ... }
+ */
+function unpackSprite( absolutePathToSprite ) {
+	if ( !unpackedSprites[absolutePathToSprite] ) {
+		unpackedSprites[absolutePathToSprite] = unpackSpriteUncached( absolutePathToSprite );
+	}
+
+	return unpackedSprites[absolutePathToSprite];
+}
+
+/**
+ * Uncached version of unpackSprite().
+ * @param {string} absolutePathToSprite
+ * @return {Object} Format: { "codeOfFrame1": filename1, "codeOfFrame2": filename2, ... }
+ */
+function unpackSpriteUncached( absolutePathToSprite ) {
+	if ( !absolutePathToSprite[0] ) {
+		throw new Error( 'unpackSprite() expects an absolute path.' );
+	}
+
+	// Load the .frames file, which contains information about size and count of individual images.
+	var framesConfPath = absolutePathToSprite.replace( /\.[^.]+$/, '.frames' );
+	var framesConf;
+	try {
+		framesConf = util.loadModFile( framesConfPath );
+	} catch ( error ) {
+		// This is not a sprite.
+		// TODO: consider treating the entire image as 1 frame called "idle" (default frame),
+		// because there are items (e.g. lunariwhip.activeitem) that use "idle" frame of non-sprite.
+		// For example:
+		// return { 'idle': absolutePathToSprite };
+
+		util.log( '[warn] .frames file not loaded: ' + framesConfPath + ': ' + error.message );
+		return {};
+	}
+
+	if ( !framesConf.frameGrid ) {
+		// Not yet implemented. Currently we only support "frameGrid" sprites.
+		// TODO: add support for "frameList" sprites.
+		return {};
+	}
+
+	// Create the directory for output files.
+	var cachePath = util.tmpdir + '/sprite' + absolutePathToSprite;
+	fs.mkdirSync( cachePath, { recursive: true } );
+
+	var grid = framesConf.frameGrid,
+		[ width, height ] = grid.dimensions
+
+	var result = childProcess.spawnSync(
+		config.imageMagickConvertCommand,
+		[
+			'-crop',
+			width + 'x' + height + '@',
+			'+repage',
+			'+adjoin',
+			absolutePathToSprite, // Input filename
+			cachePath + '/icon%d.png' // Output filename pattern
+		],
+		{ encoding: 'utf8' }
+	);
+	if ( result.status !== 0 ) {
+		console.log( result.stderr );
+		console.log( '[error] Failed to unpack the sprite: ' + absolutePathToSprite );
+		return {};
+	}
+
+	var pathsToFrames = {};
+	for ( var [ index, frameName ] of Object.entries( ( grid.names || [] ).flat() ) ) {
+		var path = cachePath + '/icon' + index + '.png';
+
+		// Frame can be referred to by:
+		// 1) name (e.g. "openRight.2"), 2) index (e.g. 42), 3) alias (see below).
+		pathsToFrames[frameName] = path;
+		pathsToFrames[index] = path;
+	}
+
+	// Apply aliases, if any. For example, the following entry: { "default.off": "off" }
+	// means that frame "off" (which we already found) should also be findable as "default.off".
+	for ( var [ alias, origFrameName ] of Object.entries( grid.aliases || {} ) ) {
+		pathsToFrames[alias] = pathsToFrames[origFrameName];
+	}
+
+	return pathsToFrames;
+}
 
 /**
  * Discover the full path (e.g. /usr/src/FrackinUniverse/items/generic/crafting/algaegreen.png)
@@ -37,12 +131,6 @@ function locateImage( relativePath, relativeToAsset ) {
 	}
 
 	var [ path, frame ] = relativePath.split( ':' );
-	if ( frame ) {
-		// We don't support frames yet ("something.png:4").
-		// TODO: use ImageMagick (convert tool) to unpack sprites and extract ":4" subimage from them.
-		return false;
-	}
-
 	if ( path[0] !== '/' ) {
 		// This path is relative to the asset (e.g. "object" file that has "inventoryIcon" key),
 		// not to the topmost directory of vanilla/mod.
@@ -62,6 +150,10 @@ function locateImage( relativePath, relativeToAsset ) {
 	for ( var possiblePath of pathCandidates ) {
 		if ( fs.existsSync( possiblePath ) ) {
 			// Found the image!
+			if ( frame ) {
+				possiblePath = unpackSprite( possiblePath )[frame] || false;
+			}
+
 			return possiblePath;
 		}
 	}
