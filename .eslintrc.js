61ad70fdfb0c82767a62142e86bebe41cd2c250d
Edward Chernenko <edwardspec@gmail.com>
2020-12-21 18:32:09 +0300

    (eslint) Add some codestyle requirements from "eslint-config-wikimedia"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,46 @@
 module.exports = {
-	"env": {
-		"node": true,
-		"es2020": true
+	env: {
+		node: true,
+		es2020: true
 	},
-	"extends": "eslint:recommended",
-	"parserOptions": {
-		"ecmaVersion": 11,
-		"ecmaFeatures": {
-			"impliedStrict": true
+	extends: [
+		'wikimedia',
+		'wikimedia/node',
+		'wikimedia/language/es2020'
+	],
+	parserOptions: {
+		ecmaVersion: 11,
+		ecmaFeatures: {
+			impliedStrict: true
 		}
 	},
-	"rules": {
-		"semi": "error"
+	rules: {
+		// This must be fixed.
+		camelcase: 'off',
+		'no-loop-func': 'off',
+		'no-new-wrappers': 'off',
+		'max-statements-per-line': 'off',
+		'no-return-assign': 'off',
+		'no-shadow': 'off',
+		'no-tabs': 'off',
+		'no-underscore-dangle': 'off',
+		'no-use-before-define': 'off',
+
+		// Might enable this later.
+		eqeqeq: 'off',
+
+		// Necessary to skip, e.g. process.exit() may be necessary for linter.
+		'mediawiki/valid-package-file-require': 'off',
+		'no-console': 'off',
+		'no-process-exit': 'off',
+
+		// Don't want to apply these.
+		'array-bracket-spacing': 'off',
+		'computed-property-spacing': 'off',
+		'max-len': 'off',
+		'no-multiple-empty-lines': 'off',
+		'no-var': 'off',
+		'one-var': 'off',
+		'vars-on-top': 'off'
 	}
 };
