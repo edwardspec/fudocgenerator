06a6e85767e0387ad7838cd5add4b93c2938077e
Edward Chernenko <edwardspec@gmail.com>
2021-01-28 23:52:28 +0300

    (eslint) Disable "prefer-const" and "no-shadow" rules (for now?)
    While the reason for these rules is valid, in this current situation
    modifying the code in way that wouldn't break them will result in
    significant decrease of readability.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,15 +21,16 @@ module.exports = {
 		eqeqeq: 'off',
 		'no-loop-func': 'off', // Only matters for async callbacks, many false positives for synchronous.
 
-		// Default is { "destructuring": "any" }, which is annoying in "for ( let [ key, value ] of ... )" loops,
-		// where only one of key/value might be eligible for "const".
-		'prefer-const': [ 'error', { destructuring: 'all' } ],
-
 		// Necessary to skip, e.g. process.exit() may be necessary for linter.
 		'mediawiki/valid-package-file-require': 'off',
 		'no-console': 'off',
 		'no-process-exit': 'off',
 		'jsdoc/no-undefined-types': 'off', // Don't want to add unnecessary require() just to use a class in JSDoc annotation
+
+		// Not exactly against applying these in the future,
+		// but they are very annoying with the current codestyle ("for ( var [ a, b ] of ..." loops, etc.).
+		'prefer-const': 'off',
+		'no-shadow': 'off',
 
 		// Don't want to apply these.
 		'array-bracket-spacing': 'off',
