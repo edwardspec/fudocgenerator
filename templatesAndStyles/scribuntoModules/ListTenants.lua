a97ea628d888929cfef87869b3ce983785a6e21c
Edward Chernenko <edwardspec@gmail.com>
2021-09-29 05:20:27 +0300

    Add list of tenants to ColonyTag categories

--- templatesAndStyles/scribuntoModules/ListTenants.lua
+++ templatesAndStyles/scribuntoModules/ListTenants.lua
@@ -3,7 +3,10 @@ local cargo = mw.ext.cargo
 
 -- Print the list of all tenants in the game. (based on [[Special:CargoTables/tenant]])
 -- Usage: {{#invoke: ListTenants|ListAllTenants}}
-function p.ListAllTenants()
+-- or {{#invoke: ListTenants|ListAllTenants|SomeColonyTag}}
+function p.ListAllTenants( frame )
+	local colonyTag = frame.args[1]
+
 	-- Perform a SQL query to the Cargo database.
 	local tables = 'tenant'
 	local fields = 'name,tagsWikitext,rentPool'
@@ -11,7 +14,16 @@ function p.ListAllTenants()
 		limit = 5000,
 		orderBy = 'name'
 	}
+
+	if colonyTag then
+		queryOpt.where = 'tags HOLDS "' .. colonyTag .. '"'
+	end
+
 	local rows = cargo.query( tables, fields, queryOpt ) or {}
+	if #rows == 0 then
+		-- No tenants require this colony tag.
+		return ''
+	end
 
 	-- Show a table of all tenants
 	-- Resistances must be in the same order as displayed in-game.
