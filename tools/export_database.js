a5d1ca6cde86c78624edbc68c4705f9fa6c123f3
Edward Chernenko <edwardspec@gmail.com>
2021-12-17 10:07:32 +0300

    (tools) Simplify [export_database.js], include filename

--- tools/export_database.js
+++ tools/export_database.js
@@ -1,20 +1,20 @@
 /**
- * Export part of the database object (such as LiquidDatabase, MaterialDatabase, etc.)
+ * Export some entities in AssetDatabase (liquids, materials, etc.)
  * as JSON string suitable for: RestoredDatabase = new Map( JSON.parse( RESULT ) )
  * ... where only several fields of entities are saved, and other fields are omitted.
  *
  * Usage:
- * node export_database.js LiquidDatabase knownLiquids name,liquidId,itemDrop,interactions
- * node export_database.js MaterialDatabase knownMaterials materialId,materialName,itemDrop,liquidInteractions
+ * node export_database.js liquid name,liquidId,itemDrop,interactions
+ * node export_database.js material materialId,materialName,itemDrop,liquidInteractions
  */
 
 'use strict';
 
-const process = require( 'process' ),
-	argv = require( 'minimist' )( process.argv.slice( 2 ) ),
-	lib = require( '../lib' );
+const { AssetDatabase } = require( '../lib' ),
+	process = require( 'process' ),
+	argv = require( 'minimist' )( process.argv.slice( 2 ) );
 
-if ( argv._.length !== 3 ) {
+if ( argv._.length !== 2 ) {
 	var usage = 'Usage: node export_database.js DatabaseName MapName Field1,Field2,Field3\n\n' +
 		'Options:\n\t--vanilla\t\tLoad only vanilla assets (without any patches)\n';
 
@@ -22,45 +22,21 @@ if ( argv._.length !== 3 ) {
 	process.exit( 1 );
 }
 
-const [ databaseClass, mapName, fieldsArg ] = argv._,
+const [ assetType, fieldsArg ] = argv._,
 	fields = fieldsArg.split( ',' );
 
-if ( databaseClass === 'AssetDatabase' || databaseClass === 'ItemDatabase' ) {
-	process.stderr.write( 'Asset/Item databases are not supported.\n' );
-	process.exit( 1 );
-}
-
-const database = require( '../lib' )[databaseClass];
-if ( !database ) {
-	process.stderr.write( 'Unknown database: ' + databaseClass + '\n' );
-	process.exit( 1 );
-}
-
-const mapToExport = database[mapName];
-if ( !mapToExport ) {
-	process.stderr.write( 'Unknown map: ' + databaseClass + '.' + mapName + '\n' );
-	process.exit( 1 );
-}
-
-if ( argv.vanilla ) {
-	// If we only need to export vanilla liquids, etc.,
-	// then inform AssetDatabase before it starts loading anything.
-	lib.AssetDatabase.enableVanillaOnlyMode();
-}
-
-// Explicitly load(), because we are not using forEach (it wouldn't tell us the primary field for the key).
-database.load();
+AssetDatabase.load( { vanillaOnly: argv.vanilla } );
 
 var serializedEntities = [];
 
-for ( var [ key, entity ] of mapToExport ) {
+AssetDatabase.forEach( assetType, ( filename, asset ) => {
 	// Export only the requested fields, nothing else.
-	var shortenedEntity = {};
+	var entity = {};
 	for ( var field of fields ) {
-		shortenedEntity[field] = entity[field];
+		entity[field] = asset.data[field];
 	}
 
-	serializedEntities.push( JSON.stringify( [ key, shortenedEntity ] ) );
-}
+	serializedEntities.push( JSON.stringify( [ filename, entity ] ) );
+} );
 
-console.log( '[\n\t' + serializedEntities.sort().join( ',\n\t' ) + '\n]' );
+console.log( '[\n' + serializedEntities.sort().join( ',\n' ) + '\n]' );
