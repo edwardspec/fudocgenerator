a31959d760cf5e035efdc7a15eb8a6a2f5fc393c
Edward Chernenko <edwardspec@gmail.com>
2021-12-24 09:54:13 +0300

    (tools) Add [make_items_cache.js]

---
+++ tools/make_items_cache.js
@@ -0,0 +1,52 @@
+/**
+ * Make a cache of { itemCode: { shortdescription: "string", icon: "string" } } for all items
+ * for the purpose of using it from Lua.
+ *
+ * Usage:
+ * node make_items_cache.js
+ * node make_items_cache.js --vanilla
+ */
+
+'use strict';
+
+const { argv, AssetDatabase } = require( '../lib' ),
+	process = require( 'process' ),
+	nodePath = require( 'path' );
+
+if ( argv.help ) {
+	let usage = 'Usage: node make_items_cache.js\n\nOptions:' +
+		'\n\t--vanilla   Load only vanilla assets (without any patches)' +
+		'\n';
+
+	process.stderr.write( usage );
+	process.exit( 1 );
+}
+
+AssetDatabase.load( { vanillaOnly: argv.vanilla } );
+
+let result = {};
+
+AssetDatabase.forEach( 'item', ( filename, asset ) => {
+	const data = asset.data,
+		itemCode = data.itemName || data.objectName;
+
+	if ( !itemCode ) {
+		return;
+	}
+
+	var icon = data.inventoryIcon;
+	if ( Array.isArray( icon ) ) {
+		icon = icon[0].image;
+	}
+
+	if ( icon && icon[0] !== '/' ) {
+		icon = nodePath.dirname( filename ) + '/' + icon;
+	}
+
+	result[itemCode] = {
+		name: data.shortdescription,
+		icon: icon
+	};
+} );
+
+console.log( JSON.stringify( result, null, '\t' ) );
