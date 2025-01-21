d7b989933d3224aab9586be1436d5434de9b899d
Edward Chernenko <edwardspec@gmail.com>
2025-01-21 11:05:32 +0300

    (tools) Price of all nodes: count all items (not just currencies)

--- tools/count_price_of_all_research_nodes.js
+++ tools/count_price_of_all_research_nodes.js
@@ -1,30 +1,60 @@
 /**
- * Count the sum of Research currency that is needed to unlock everything.
+ * Count the sum of Research currency and other items that are needed to unlock everything.
+ * Takes into account that items are NOT consumed, only currencies.
  */
 
 'use strict';
 
-const { ResearchTreeDatabase } = require( '../lib' );
+const { ItemDatabase, ResearchTreeDatabase, RecipeSide } = require( '../lib' );
 
-let costByItem = {};
+const isConsumed = {
+	// List of resources that are consumed when unlocking the node.
+	fuscienceresource: true,
+	fumadnessresource: true,
+	fugeneticmaterial: true,
+	essence: true,
+	money: true
+};
+
+let itemCount = {}; // { itemCode => count, ... }
 
 ResearchTreeDatabase.forEach( ( node ) => {
 	node.price.getAllComponents().forEach( ( component ) => {
-		let count = component.quantity.count;
-		if ( !count ) {
-			return;
-		}
+		let count = component.quantity.count,
+			itemCode = component.code;
 
-		if ( !costByItem[component.code] ) {
-			costByItem[component.code] = 0;
-		}
+		if ( count && component.isItem ) {
+			if ( !itemCount[itemCode] ) {
+				itemCount[itemCode] = 0;
+			}
 
-		costByItem[component.code] += count;
+			if ( isConsumed[itemCode] ) {
+				itemCount[itemCode] += count;
+			} else {
+				itemCount[itemCode] = Math.max( count, itemCount[itemCode] );
+			}
+		}
 	} );
 } );
 
-console.log(
-	( costByItem.fuscienceresource || 0 ) + ' of Research currency, ' +
-	( costByItem.fumadnessresource || 0 ) + ' of Madness currency, ' +
-	( costByItem.fugeneticmaterial || 0 ) + ' of Genes currency are needed to unlock ALL nodes.'
-);
+const itemCountSorted = Object.entries( itemCount ).sort( ( a, b ) => {
+	// Sort by quantity: from more to less.
+	const countDiff = b[1] - a[1];
+	if ( countDiff !== 0 ) {
+		return countDiff;
+	}
+
+	// Count is the same, sort alphabetically.
+	const name1 = ItemDatabase.getDisplayName( a[0] ) || '',
+		name2 = ItemDatabase.getDisplayName( b[0] ) || '';
+	return name1.localeCompare( name2 );
+} );
+
+// Print all required items.
+let totalCost = new RecipeSide();
+for ( let [ itemCode, count ] of itemCountSorted ) {
+	totalCost.addItem( itemCode, { count: count } );
+}
+
+ItemDatabase.load();
+console.log( 'Necessary amount of Research and items to unlock ALL nodes:\n' + totalCost.toWikitext() );
